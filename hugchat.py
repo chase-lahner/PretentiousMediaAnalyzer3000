10e6528980f666e9acb86474b8c9dc5df058df00
chase-lahner <chasealahnerphone@gmail.com>
2024-03-30 01:16:01 -0400

    merge

--- hugchat.py
+++
@@ -1,6 +0,0 @@
-from json import loads
-
-from hugchat import hugchat
-from hugchat.login import Login
-from transformers import pipeline
-
--- spot.py
+++ spot.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request, session, redirect, url_for
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FlaskSessionCacheHandler
+import hugg
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
@@ -57,7 +58,20 @@ def get_playlists():
     playlists_info = [(pl['name'], pl['external_urls']['spotify'])
                       for pl in playlists['items']]
     playlists_html = '<br>'.join([f'{name}:{url}' for name, url in playlists_info])
-    return playlists_html
+    return jsonify(playlists_html)
+
+
+def get_songs1(songs: str) -> str:
+    biasPrompt = "Pretend that you are a pretentious record store owner. You only like music that says something profound about the person who listens to it. The more obscure the artist, the better. This means that you prefer songs with fewer listens. You will be given a list of 50 songs, and deduce how much you like the collection as a whole, giving it a score of 0-10. 10 is a high score, meaning you liked it, while 0 means you did not. Be SUPER PRETENTIOUS. Give an integer as the first word of the answer. If you do not give an integer first, the machine will break."
+    hugbot = hugg.hugCommands("rorycavanmc@gmail.com", "wnVmBwCsUuThM2")
+    rating  = hugbot.pretentious_score(biasPrompt, songs)
+
+    
+
+    return rating
+
+    
+
 
 
 @app.route('/get_songs')
@@ -68,15 +82,13 @@ def get_songs():
     songs = sp.current_user_top_tracks(limit=50)
     song_info = [(pl['name'], pl['artists'][0]['name'])
                 for pl in songs['items']]
-    songs_html = '<br>'.join([f'{name}:{artist}' for name, artist in song_info])
-
-    return songs_html
-                 
-                 
-
+    songs_html = '<br>'.join([f'{name}:{artist}' for name, artist in song_info]) + "\n"
     
-                   
-    return jsonify(songs)
+    
+
+    return songs_html  + '<br />' + get_songs1(songs_html)
+                 
+                 
 
     
 
