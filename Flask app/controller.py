389d3105abae648fd6446f955dfb9ce66ff68e81
Ivan Puri <isp25@pitt.edu>
2024-03-30 00:36:40 -0400

    first

--- Flask app/controller.py
+++ Flask app/controller.py
@@ -31,56 +31,12 @@ def home():
         return redirect(auth_url)
     return redirect(url_for('poop'))
 
-
 @app.route('/homefr')
 def homefr():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
     return "welcome to my page <a href='/get_songs'>get top_songs</a>"
-
-
-
-@app.route('/callback')
-def callback():
-    sp_oauth.get_access_token(request.args['code'])
-    return redirect(url_for('homefr'))
-
-
-@app.route('/get_playlists')
-def get_playlists():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-
-    playlists = sp.current_user_playlists()
-    playlists_info = [(pl['name'], pl['external_urls']['spotify'])
-                      for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}:{url}' for name, url in playlists_info])
-    return playlists_html
-
-
-@app.route('/get_songs')
-def get_songs():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    songs = sp.current_user_top_tracks(limit=50)
-    song_info = [(pl['name'], pl['artists'][0]['name'])
-                for pl in songs['items']]
-    songs_html = '<br>'.join([f'{name}:{artist}' for name, artist in song_info])
-
-    return songs_html
-                 
-                 
-
-    
-                   
-    return jsonify(songs)
-
-    
-
-
 
 @app.route('/logout')
 def logout():
@@ -91,5 +47,3 @@ def logout():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
     
-
-
